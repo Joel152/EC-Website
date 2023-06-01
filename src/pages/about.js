258b1e832f8905f0c0fbdb74a6f98bfb44f2b992
Joel152 <135132469+Joel152@users.noreply.github.com>
2023-06-01 12:14:48 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -69,19 +69,12 @@ const AboutPage = (props) => {
 
         <Container size={'large'} spacing={'min'}>
           <div className={styles.content}>
-            <h3>Our Values</h3>
+            <h3>In-Game Lore</h3>
             <div ref={valuesRef}>
               <p>
-                Sunspel produced some of the world's earliest T-shirts. In the
-                late 1800s the business made luxury tunics and undershirts from
-                lightweight Sea Island cotton for export to the Far East and
-                other warm climates. While these garments initially had silk
-                buttoned plackets, these were removed in the early 1900s and
-                replaced with simple bound necks to reduce manufacturing costs -
-                creating the T-shirt. We've supplied the world as the T-shirt
-                has evolved from underwear to outerwear, from symbol of youthful
-                rebellion to everyday wardrobe staple, and we've spent decades
-                refining its every last aspect.
+                The Ethics Committee will host regular inspections all around the site. 
+                This includes some of the Anomaly Containment Chambers, all of the Sectors 
+                and Class-D Containment Cells along with Omega-1 assisting them for safety.
               </p>
               <ol>
                 <li>Be an ecowear</li>
