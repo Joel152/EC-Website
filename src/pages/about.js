e4b654c8beb2d3198a8ebc9024cf9a013b4ae07c
Joel152 <135132469+Joel152@users.noreply.github.com>
2023-06-01 14:10:42 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -64,7 +64,7 @@ const AboutPage = (props) => {
         </Container>
 
         <div className={styles.imageContainer}>
-          <img alt={'shirt brand'} src={'/EC about 4.png'}></img>
+          <img alt={'shirt brand'} src={'/About EC role 4.png'}></img>
         </div>
 
         <Container size={'large'} spacing={'min'}>
