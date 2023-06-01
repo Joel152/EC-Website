3d5b96f98131cc1212f1f38c34522bf3358d33f1
Joel152 <135132469+Joel152@users.noreply.github.com>
2023-06-01 12:16:34 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = (props) => {
         <Hero
           maxWidth={'900px'}
           image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          title={`The Ethics Committee`}
         />
 
         <div className={styles.navContainer}>
